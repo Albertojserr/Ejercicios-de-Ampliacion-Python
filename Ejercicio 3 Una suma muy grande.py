eb1667ba7e50e288b16c84fa0169f39883d708e6
Albertojserr <alberto.j.serrano@gmail.com>
2021-11-01 14:44:48 +0100

    Ejercicios 3 y 4

---
+++ Ejercicio 3 Una suma muy grande.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Write your code here
+    suma = 0
+    for i in ar:
+        suma+=i
+    return suma
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'] + 'solucion3.txt', 'w')
+    print("Escribe un número")
+    ar_count = int(input().strip())
+    print("Escribe la lista de números")
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ Ejercicio 4 La piramide.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    # Write your code here
+    for i in range(0,n):
+        linea=""
+        for k in range(0,n-1-i):
+            linea=linea + " "
+        for j in range(0,i+1):
+            linea = linea + "# "
+        print(linea)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
