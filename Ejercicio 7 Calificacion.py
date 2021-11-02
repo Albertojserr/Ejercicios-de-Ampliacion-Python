6ebbe45ba55d38c1d94ce918ca0396b25768960d
Albertojserr <alberto.j.serrano@gmail.com>
2021-11-02 14:25:39 +0100

    Ejercicio 7

---
+++ Ejercicio 7 Calificacion.py
@@ -0,0 +1,49 @@
+import math
+import os
+import random
+import re
+import sys
+#
+10. # Complete the 'gradingStudents' function below.
+11. #
+12. # The function is expected to return an INTEGER_ARRAY.
+13. # The function accepts INTEGER_ARRAY grades as parameter
+14. #
+
+def gradingStudents(grades):
+    # Write your code here
+    list=[]
+    for grade in (grades):
+        list.append(finalgrade(grade))
+    return list
+
+def finalgrade(grades):
+    roundgrade=0
+    if(grades<=37):
+        roundgrade=grades
+    else:
+        cociente=int(grades/5 + 1)
+        multiplo=cociente*5
+        if(multiplo-grades<3):
+            roundgrade=multiplo
+        else:
+            roundgrade=grades
+    return roundgrade
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'] + "solucion7.txt", 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
